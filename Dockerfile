29e0761b804e25f331a9029ae20e750b9c7aa011
santroma1 <santroma1@gmail.com>
2023-07-01 11:15:13 +0200

    change dockerfile, requirements and commands

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime
 WORKDIR /workspace
-ADD . .
 COPY . .
 RUN pip install -r requirements.txt
--- docker_swedish_training.sh
+++ docker_swedish_training.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 cd SweQUAD-MC/models/inlg2021/bert_based/
-python trainer.py --do_train --train_data ../../../data/training.json --output_dir output/areg/ --per_device_train_batch_size 4 --per_device_eval_batch_size 4 --num_train_epochs 8 --model KB/bert-base-swedish-cased --formulation areg
-# python3 generator.py -f output/areg/checkpoint-3000 -dg output/areg/dg_args.bin -o output/areg/res_dev_nlo_min_first.txt -d ../../../data/dev.json -nlo -ls min_first
+python trainer.py --do_train --train_data ../../../data/training.json --dev_data ../../../data/dev.json --save_steps 2000 --evaluation_strategy steps --eval_steps 2000 --output_dir output/aureg/ --metric_for_best_model eval_loss --per_device_train_batch_size 4 --per_device_eval_batch_size 4 --num_train_epochs 6 --model KB/bert-base-swedish-cased --formulation areg
+
+python3 generator.py -f output/aureg/checkpoint-12000 -dg output/aureg/dg_args.bin -o output/aureg/gen/res_dev_nlo_min_first -d ../../../data/dev.json -nlo -ls min_first
+
+python3 evaluator.py -f output/aureg/gen/res_dev_nlo_min_first-18000.txt -t ../../../data/training.json --output-dir output/aureg/gen/ --pretrained KB/bert-base-swedish-cased
+
+
+python trainer.py --do_train --train_data ../../../data/training.json --dev_data ../../../data/dev.json --save_steps 2000 --evaluation_strategy steps --eval_steps 2000 --output_dir output/upmlm/ --metric_for_best_model eval_loss --per_device_train_batch_size 4 --per_device_eval_batch_size 4 --num_train_epochs 6 --model KB/bert-base-swedish-cased --formulation upmlm
+
+python3 generator.py -f output/upmlm/checkpoint-16000 -dg output/upmlm/dg_args.bin -o output/upmlm/gen/res_dev_nlo_min_first -d ../../../data/dev.json -nlo -ls min_first
+
+python3 evaluator.py -f output/upmlm/gen/res_dev_nlo_min_first-16000.txt -t ../../../data/training.json --output-dir output/upmlm/gen/ --pretrained KB/bert-base-swedish-cased
 # python trainer.py -h
